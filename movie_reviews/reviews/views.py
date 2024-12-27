4baf5d022beae1e6dfdb578f37b9fc18a5f03632
sandeep21r <108021509+sandeep21283@users.noreply.github.com>
2024-12-27 13:43:02 +0530

    new change
    .

--- movie_reviews/reviews/views.py
+++ movie_reviews/reviews/views.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import MovieReview
 from .serializers import MovieReviewSerializer
-from .permissions import IsReviewerOrReadOnly
 
 class MovieReviewViewSet(viewsets.ModelViewSet):
  
